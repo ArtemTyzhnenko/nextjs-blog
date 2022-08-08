586259037db44c2de1e79474fb3198b29f928d01
Artem Tyzhnenko <artemtyzhnenko@Artems-MacBook-Pro.local>
2022-08-08 17:23:41 +0200

    test 2

--- pages/index.js
+++ pages/index.js
@@ -5,8 +5,6 @@ import { getSortedPostsData } from '../lib/posts';
 import Link from 'next/link';
 import Date from '../components/date';
 
-//test
-
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
   return {
